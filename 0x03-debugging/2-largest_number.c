46d5ce85480be0ecf449b14a0bda76c2f51df713
Isoken <isokenjune@gmail.com>
2023-06-22 13:48:13 -0700

    printing largest number function

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -8,7 +9,7 @@
  * Return: largest number
  */
 
-int largest_number(int a, int b , int c )
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
