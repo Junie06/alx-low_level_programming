21d0c2ef3a78616ab345fa51b1d16a3d1456523a
Isoken <isokenjune@gmail.com>
2023-06-22 16:22:15 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,14 +4,13 @@
  * mul - Function that multiplies two integers
  * @a: first argument
  * @b: second argument passed
- * @c: result
  * Return: c (Success)
  */
 int mul(int a, int b)
 {
 	int c;
 
-	c = a*b;
+	c = a * b;
 
 	return (c);
 }
