e4e30061ffb7d81c94cd7d2202a95d5793300988
Isoken <isokenjune@gmail.com>
2023-06-22 16:53:24 -0700

    looping

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *
+ * Return: null
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && != 4)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
