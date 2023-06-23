eeeddcd8660f41da6544bcdae8363b20ca3c4329
Isoken <isokenjune@gmail.com>
2023-06-23 07:14:57 -0700

    printing diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,15 +7,20 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
-	for (i = 0; i < n; i++)
-	{
-		_putchar('\\');
-		_putchar('\n');
-	}
 	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
