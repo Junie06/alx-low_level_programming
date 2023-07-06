57b858a0ff6e3c50a49533b414a1f0cbdc5144bb
Isoken <isokenjune@gmail.com>
2023-07-06 14:46:39 -0700

    pushing 6 prime file again

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,5 +10,20 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 	return (-1);
 
-	return (_sqrt_recursion(n));
+	return (_recursion_calc(n, 1));
+}
+
+/**
+ * _recursion_calc - function to help calculate sqrt
+ * @n: number to calculate sqrt
+ * @num: iterator
+ * Return: resulting sqrt
+ */
+int _recursion_calc(int n, int num)
+{
+	if (num * num == n)
+		return (num);
+	if (num * num > n)
+		return (-1);
+	return (_recursion_calc(n, num + 1));
 }
