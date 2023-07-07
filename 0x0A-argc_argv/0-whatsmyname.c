be0e0dc5d02fcecf784bf82f57cea3d5664a30a2
Isoken <isokenjune@gmail.com>
2023-07-07 14:24:29 -0700

    program that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: count
+ * @argv: vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%s\n", argv[0]);
+	return (0);
+}
