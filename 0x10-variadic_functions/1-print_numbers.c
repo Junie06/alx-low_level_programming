7649bc6b1069ce41893a7546be6b0e597f42e396
Isoken <isokenjune@gmail.com>
2023-07-21 12:54:50 -0700

    corrections to variadic function tasks

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,13 +12,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 
 	va_start(list, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		if (separator)
 			printf("%d%s", va_arg(list, int), separator);
+		else if (separator && i == 0)
+		printf("%d", va_arg(list, int));
 		else
 			printf("%d", va_arg(list, int));
 	}
-	printf("%d\n", va_arg(list, int));
+	printf("\n");
 	va_end(list);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,25 +13,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	va_start(strings, n);
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
-		if (separator)
-		{
-			if (!str)
-				printf("(nil)%s", separator);
-			else
-				printf("%s%s", str, separator);
-		}
+		if (!str)
+			str = "(nil)";
 		if (!separator)
-		{
-			if (!str)
-				printf("(nil)");
-			else
-				printf("%s", str);
-		}
+			printf("%s", str);
+		else if (separator && i == 0)
+		printf("%s", str);
+		else
+			printf("%s%s", separator, str);
 	}
-	printf("%s", va_arg(strings, char *));
 	printf("\n");
 	va_end(strings);
 }
