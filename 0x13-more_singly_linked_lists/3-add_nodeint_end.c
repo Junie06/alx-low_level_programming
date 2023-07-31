ff1cb232d80de919d19a7100207e5e087fe959f3
Isoken <isokenjune@gmail.com>
2023-07-31 14:50:50 -0700

    corrections to the fourth and fifth tasks

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,7 +9,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node = NULL,
+	listint_t *new_node = NULL;
 	listint_t *temp = *head;
 
 	new_node = malloc(sizeof(listint_t));
