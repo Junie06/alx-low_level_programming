a4115155b59a336981b63e2b9f1cbb684a0b73b3
Isoken <isokenjune@gmail.com>
2023-08-04 12:29:39 -0700

    correction to bit manipulation tasks

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,16 +8,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0, i = 0;
+	unsigned int result = 0;
 
 	if (!b)
 		return (0);
-	while (*b)
+	while (*b != '\0')
 	{
 		if (*b != '0' && *b != '1')
+		{
 			return (0);
-		result = result * 2 + (*b - '0');
-		i++;
+		}
+		result <<= 1;
+		result |= (*b - '0');
+		b++;
 	}
 	return (result);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,21 @@
 #include "main.h"
-
 /**
  * print_binary - function that converts a decimal to binary
  * @n: value passed as argument
  * Return: the converted  value else 0
  */
-
 void print_binary(unsigned long int n)
 {
 	int i, bits, lead_zero = 1;
 
-	/**
-	  *calculates the size of the data
-	   * type in bytes and coverts it to
-	   * number of bits
-	   */
+	/*calculates the size of the data in bytes and coverts it to number of bits*/
 	bits = sizeof(unsigned long int) * 8;
 
 	for (i = bits - 1; i >= 0; i--)
 	{
 		/* extract the i-th bit*/
 		bits = (n >> i) & 1;
-		
+
 		if (bits)
 		{
 			lead_zero = 0;
@@ -33,5 +27,5 @@ void print_binary(unsigned long int n)
 		}
 	}
 	if (lead_zero)
-	_putchar('0');
+		_putchar('0');
 }
