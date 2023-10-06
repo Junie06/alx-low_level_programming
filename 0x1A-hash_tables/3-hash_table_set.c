1e85dac34bf0a7733b0f4cfce4ba338bccd74745
Isoken <isokenjune@gmail.com>
2023-10-06 16:23:21 -0700

    corrections for 0 & 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,25 +13,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *value_copy = NULL, *key_copy = NULL;
 	unsigned long int index;
 
-	if (!ht || !key)
+	if (!ht || !key || *key == '\0' || !value)
 		return (0);
 
 	value_copy = strdup(value);
 	if (!value_copy)
-		free(value_copy);
-	return (0);
+		return (0);
 
 	key_copy = strdup(key);
-	if (!key_copy)
-		free(key_copy);
+	if (!key_copy)i
+		free(new_node);
 	return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] && *(ht->array[index]->key) == *key)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(ht->array[index]->value);
-		ht->array[index]->value = value_copy;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[index]->value);
+			ht->array[index]->value = value_copy;
+			return (1);
+		}
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
@@ -39,12 +41,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(value_copy);
 	return (0);
 
-	new_node->key = key_copy;
 	new_node->value = value_copy;
-	new_node->next = NULL;
-
-	if (ht->array[index])
-		new_node->next = ht->array[index];
+	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
