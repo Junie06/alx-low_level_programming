70d4af36acbd8266f195c40a6a39312754f9055d
Isoken <isokenjune@gmail.com>
2023-10-11 21:50:38 -0700

    correction for 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,43 +9,43 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node = NULL;
-	char *value_copy = NULL, *key_copy = NULL;
-	unsigned long int index;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	if (!ht || !key)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	value_copy = strdup(value);
-	if (!value_copy)
-		free(value_copy);
-	return (0);
-
-	key_copy = strdup(key);
-	if (!key_copy)
-		free(key_copy);
-	return (0);
+	if (value_copy == NULL)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index] && *(ht->array[index]->key) == *key)
+	for (i = index; ht->array[i]; i++)
 	{
-		free(ht->array[index]->value);
-		ht->array[index]->value = value_copy;
-		return (1);
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
 	}
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
 		free(value_copy);
-	return (0);
-
-	new_node->key = key_copy;
-	new_node->value = value_copy;
-	new_node->next = NULL;
-
-	if (ht->array[index])
-		new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,17 +8,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	hash_node_t *node;
 	unsigned long int index;
 
-	if (!ht || !key)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-	while (ht->array[index])
-	{
-		if (*(ht->array[index]->key) == *key)
-			return (ht->array[index]->value);
-	}
-	ht->array[index] = ht->array[index]->next;
-	return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
+
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
+
+	return ((node == NULL) ? NULL : node->value);
 }
